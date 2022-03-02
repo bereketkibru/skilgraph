decfc498c06de52b79f40ce0675ec37e7a5531cb
Bereket Kibru <bekakibru2@gmail.com>
2022-03-02 05:33:17 +0300

    add hovering property and change the size of the graph

--- routes/home/components/graph.tsx
+++ routes/home/components/graph.tsx
@@ -31,21 +31,28 @@ export default function Graph({ data }) {
                   nodeAutoColorBy="selected"
                   width={500}
                   height={500}
-                  minZoom={2.5}
+                  minZoom={2  }
                   nodeCanvasObject={(node, ctx, globalScale) => {
                     const label = node.id;
-                    const fontSize = 20 / globalScale;
+                    const fontSize = 12 / globalScale;
                     ctx.font = `${fontSize}px Sans-Serif`;
                     const textWidth = ctx.measureText(label).width;
                     const bckgDimensions = [textWidth, fontSize].map(
                       (n) => n + fontSize * 0.2
                     ); // some padding
-                    ctx.textAlign = "top";
+                    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+            ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+                    ctx.textAlign = "center";
                     ctx.textBaseline = "middle";
                     ctx.fillStyle = node.color;
                     ctx.fillText(label, node.x, node.y);
 
                     node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+                  }}
+                  nodePointerAreaPaint={(node, color, ctx) => {
+                    ctx.fillStyle = color;
+                    const bckgDimensions = node.__bckgDimensions;
+                    bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
                   }}
                 />
               </Box>
